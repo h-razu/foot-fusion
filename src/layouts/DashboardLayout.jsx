d6a646731cc5fe2c6b6e08b2baa13efca65091a3
h-razu <hrazu393@gmail.com>
2024-05-26 11:26:38 +0600

    improve dashboard layout

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,25 +1,54 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const DashboardLayout = () => {
+  const activeNav = {
+    color: "#009B4D",
+    background: "none",
+  };
+
   return (
     <div className="grid grid-cols-12">
       <div className="col-span-2 bg-gray-300 min-h-screen p-12">
-        <ul>
-          <li className="border border-black p-4 text w-full">
-            <Link to={"home"}>Dashboard</Link>
+        <ul className="flex flex-col gap-y-7">
+          <li>
+            <NavLink
+              to={"home"}
+              className="text-2xl font-semibold"
+              style={({ isActive }) => (isActive ? activeNav : undefined)}
+            >
+              Dashboard
+            </NavLink>
           </li>
-          <li className="border border-black p-4 text w-full">
-            <Link to={"all-products"}>All Products</Link>
+          <li>
+            <NavLink
+              to={"all-products"}
+              className="text-2xl font-semibold"
+              style={({ isActive }) => (isActive ? activeNav : undefined)}
+            >
+              All Products
+            </NavLink>
           </li>
-          <li className="border border-black p-4 text w-full">
-            <Link to={"add-products"}>Add Product</Link>
+          <li>
+            <NavLink
+              to={"add-products"}
+              className="text-2xl font-semibold"
+              style={({ isActive }) => (isActive ? activeNav : undefined)}
+            >
+              Add Product
+            </NavLink>
           </li>
-          <li className="border border-black p-4 text w-full">
-            <Link to={"/"}>Home</Link>
+          <li>
+            <NavLink
+              to={"/"}
+              className="text-2xl font-semibold"
+              style={({ isActive }) => (isActive ? activeNav : undefined)}
+            >
+              Home
+            </NavLink>
           </li>
         </ul>
       </div>
-      <div className="col-span-10 p-20">
+      <div className="col-span-10 p-10">
         <Outlet />
       </div>
     </div>
